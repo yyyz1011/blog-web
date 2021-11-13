b13d3dafe1de6a4b72dac8cd10a08dc343ed734c
yzcode <1540032876@qq.com>
2021-11-13 19:25:31 +0800

    'feat:导航栏'

--- components/NavHeader/index.vue
+++ components/NavHeader/index.vue
@@ -2,16 +2,38 @@
   <div class="nav-header">
     <div class="content">
       <img class="logo" src="~/static/img/logo.png" @click="handleClickLogo" />
+      <div class="nav">
+        <div
+          class="nav-item"
+          :class="{ 'is-nav-active': currentName === item.name }"
+          v-for="item in NavHeader"
+          :key="item.key"
+          @click="handleNavChange(item.name)"
+        >
+          {{ item.label }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { NavHeader } from "@/constants/navHeader";
+
 export default {
   name: "NavHeader",
+  data() {
+    return {
+      NavHeader,
+      currentName: NavHeader[0].name,
+    };
+  },
   methods: {
     handleClickLogo() {
       console.log("TODO logo click");
+    },
+    handleNavChange(name) {
+      this.currentName = name;
     },
   },
 };
@@ -40,5 +62,24 @@ export default {
   height: 50%;
   cursor: pointer;
   user-select: none;
+}
+.nav {
+  display: flex;
+  align-items: center;
+  color: $text-white;
+  font-size: 20px;
+  font-weight: lighter;
+  margin-left: 20px;
+  height: 100%;
+  .nav-item {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 16px;
+    margin: 0 16px;
+    cursor: pointer;
+    user-select: none;
+  }
 }
 </style>
---
+++ constants/navHeader.js
@@ -0,0 +1,20 @@
+export const NavHeader = [
+    {
+        key: 1,
+        label: '主页',
+        name: 'home',
+        path: ''
+    },
+    {
+        key: 2,
+        label: '笔记',
+        name: 'article',
+        path: ''
+    },
+    {
+        key: 3,
+        label: '我的',
+        name: 'me',
+        path: ''
+    },
+]
