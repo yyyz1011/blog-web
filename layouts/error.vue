4c51a7e9c9c47e086f2a872ddc8839faf3feb1d6
yzcode <1540032876@qq.com>
2021-11-13 20:03:06 +0800

    'feat:增加404页面配置'

---
+++ layouts/error.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>{{ error.message }}</h1>
+</template>
+
+<script>
+export default {
+  name: "error",
+  props: ["error"],
+};
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,10 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  router: {
+    middleware: 'unknownRoute'
+  },
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
   ],
