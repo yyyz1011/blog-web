a8f8c9f1f0ccb042200fb787f0fac36a3661764a
yzcode <1540032876@qq.com>
2022-02-13 19:52:48 +0800

    feat: add article-card and article-list

---
+++ src/components/article-card/index.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Card, Tag } from "@douyinfe/semi-ui";
+import "./index.less";
+import { TagColor } from "@douyinfe/semi-ui/lib/es/tag";
+import dayjs from "dayjs";
+
+interface ArticleTag {
+  type: string;
+  color: string;
+}
+interface IProps {
+  info: {
+    id: string;
+    title: string;
+    desc: string;
+    type: Array<ArticleTag>;
+    modify_time: string;
+  };
+}
+
+const ArticleCard: React.FC<IProps> = (props: IProps) => {
+  const { title, desc, type, modify_time: modifyTime } = props.info;
+  const updateTime = dayjs(Number(modifyTime)).format("YYYY-MM-DD HH:mm");
+
+  const handleGoArticle = () => {
+    console.log("TODO go article");
+  };
+
+  return (
+    <>
+      <div className="article-card" onClick={handleGoArticle}>
+        <Card
+          bordered
+          headerLine={false}
+          shadows="hover"
+          title={
+            <div className="article-title">
+              <span className="article-title--title">{title}</span>
+              {type.length &&
+                type.map((item, index) => (
+                  <Tag
+                    key={"article-type-" + index}
+                    className="article-title--type"
+                    color={item.color as TagColor}
+                  >
+                    {item.type}
+                  </Tag>
+                ))}
+            </div>
+          }
+          headerExtraContent={
+            <div className="article-extra-title">更新时间:{updateTime}</div>
+          }
+        >
+          {desc}
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default ArticleCard;
--- src/views/article/index.tsx
+++ src/views/article/index.tsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import ArticleCard from "@/components/article-card";
+import "./index.less";
+import { List, Button } from "@douyinfe/semi-ui";
 
-const Article: React.FC = () => {
-  return (
-    <>
-      <div>articl12e</div>
-    </>
-  )
+interface ArticleTag {
+  type: string;
+  color: string;
+}
+interface ArticleItem {
+  id: string;
+  title: string;
+  desc: string;
+  type: Array<ArticleTag>;
+  modify_time: string;
 }
 
-export default Article
+const Article: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [MOCK_articleList, setArticleList] = useState<Array<ArticleItem>>([]);
+
+  const handleLoadMore = () => {
+    setLoading(true)
+    const start_index = MOCK_articleList.length;
+    const MOCK_times = start_index + 5;
+    let new_list = MOCK_articleList;
+    for (let i = start_index; i < MOCK_times; i++) {
+      new_list.push({
+        id: i.toString(),
+        title: "这是一个标题11",
+        desc: "阿巴巴爸爸",
+        type: [
+          { type: "vue", color: "purple" },
+          { type: "react", color: "red" },
+        ],
+        modify_time: new Date().getTime().toString(),
+      });
+    }
+    setArticleList(new_list);
+    setLoading(false)
+  };
+
+  useEffect(() => {
+    handleLoadMore();
+  }, []);
+
+  useEffect(() => {
+    console.log(MOCK_articleList);
+  }, [MOCK_articleList]);
+
+  return (
+    <>
+      <div className="article">
+        <div className="article-content">
+          <List
+            loading={loading}
+            dataSource={MOCK_articleList}
+            renderItem={(item) => (
+              <ArticleCard key={item.id} info={item}></ArticleCard>
+            )}
+          ></List>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Article;
