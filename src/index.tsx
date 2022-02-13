77aa1d184413d7db0f4c89e09a0e5608f957120f
yzcode <1540032876@qq.com>
2022-02-14 00:49:41 +0800

    feat: update console info

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,16 @@ import App from "@/app";
 import "@/utils/i18n";
 import "@/assets/css/common.less";
 
+const leafBlogInfo =
+  "  __                  ___   __       __                 \n" +
+  "  [  |               .' ..] [  |     [  |                \n" +
+  "   | | .---.  ,--.  _| |_    | |.--.  | |  .--.   .--./) \n" +
+  "   | |/ /__\\`'_\ :'-| |-'   | '/'`\ \| |/ .'`\ \/ /'`\; \n" +
+  "   | || \__.,// | |, | |     |  \__/ || || \__. |\ \._// \n" +
+  "  [___]'.__.'\'-;__/[___]   [__;.__.'[___]'.__.' .',__`  \n" +
+  "                                                ( ( __)) ";
+
+console.info(leafBlogInfo);
+console.log("Welcome %c Ye Zhou's %c leaf blog","color:orange;font-weight:bold","")
+
 render(<App />, document.getElementById("app"));
