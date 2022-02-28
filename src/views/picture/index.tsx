3ac2acd0e53106e3a1e02f15b6b18953c63616cb
yzcode <1540032876@qq.com>
2022-02-28 23:48:33 +0800

    feat: init axios with ts and status

--- src/views/picture/index.tsx
+++ src/views/picture/index.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getFreeImg } from "@/network";
 
 const Picture: React.FC = () => {
+  const [imgList, setImgList] = useState(null);
+
+  const getAllImg = async () => {
+    const mockLinkList = new Array(10).fill(getFreeImg);
+    const imgList = mockLinkList.map((item) => {
+      return item({
+        width: 400,
+        height: Math.floor(Math.random() * 5 + 5) * 100,
+      });
+    });
+    setImgList(imgList);
+  };
+
+  useEffect(() => {
+    getAllImg();
+  }, []);
+
   return (
     <>
-      <div>picture</div>
+      <div>
+        {imgList &&
+          imgList.map((item: string, index: number) => (
+            <img src={item} key={index}></img>
+          ))}
+      </div>
     </>
   );
 };
